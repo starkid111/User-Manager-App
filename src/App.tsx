6ea6abab2287cb4a26e8c9eab8df29d31ad511de
sodiq-wiuc <200686523+sodiq-wiuc@users.noreply.github.com>
2025-10-14 15:56:19 +0100

    fetched Api from a fake rest API

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+
+import { useEffect, useState } from "react";
+
+import { fetchUsers, type User } from "./utils/api";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [users, setUsers] = useState<User[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
 
+  useEffect(() => {
+    const fetchdata = async () => {
+      setLoading(true);
+      try {
+        const data = await fetchUsers();
+        setUsers(data);
+      } catch (error) {
+        setError("Failed to fetch users");
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchdata();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>User Manager App</h1>
+        <div>
+          {users.map((user) => {
+            return (
+              <div key={user.id}>
+                {user.name}
+              </div>
+            );
+          })}
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/utils/api.tsx
@@ -0,0 +1,39 @@
+import axios from "axios";
+export interface User {
+    id?: number;
+    name: string;
+    username: string;
+    email: string;
+    phone: string;
+    website: string;
+}
+const api = axios.create({
+    baseURL:"https://api.restful-api.dev",
+    headers: {
+        "Content-Type": "application/json"
+    }
+    
+})
+
+
+export const fetchUsers = async (params? : any ) : Promise<User[]> => {
+    try {
+        const response = await api.get("/objects", {params});
+        return response.data;
+    }
+    catch (error) {
+        console.error("Error fetching users:", error);
+        throw error;
+    }
+} 
+
+
+export const  addUsers = async (user : User) : Promise<User> => {
+    try {
+        const response = await api.post("/users", user)
+        return response.data;
+    }catch (error) {
+        console.error("Error adding user:", error);
+        throw error;
+    }
+}
